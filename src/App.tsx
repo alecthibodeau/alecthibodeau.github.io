7879e2779f41bcf477b44cd9078f68d70a53e7e9
alec t <alec@alect.me>
2022-07-24 17:20:03 -0400

    Add homepage property to package lock file

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="app">
         <Routes>
           <Route path="/" element={<CodePortfolio />} />
