49ab5128e1962ebe5c5bf1bd1592b3db06b59ae8
alec t <alec@alect.me>
2023-07-21 19:01:09 -0400

    Rewrite descriptive text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,17 +29,14 @@ function Footer(): JSX.Element {
     <footer>
       <div className="banner-04"></div>
       <section id={constants.sections.information}>
-        <div className="section-title">{helpers.formatTitleCase(constants.sections.information)}</div>
+        <div className="section-title">
+          {helpers.formatTitleCase(constants.sections.information)}
+        </div>
         <div className="information-box">
           {constants.informationItems.map(renderInformationItem)}
         </div>
-        <div>
-          <p>
-            I design and maintain this code portfolio site with React, TypeScript
-            and Sass (<a className="text-link" href={constants.hypertextReference.gitHub}>see
-            the code at GitHub</a>). Scroll up for more <a className="text-link" href="#projects">projects</a>.
-          </p>
-          <div className="credit">{`Alec Thibodeau, ${new Date().getFullYear()}`}</div>
+        <div className="credit">
+          {`Alec Thibodeau, ${new Date().getFullYear()}`}
         </div>
         <div className="stripes-container">
           {Array(7).fill('stripe').map((item: string, index: number) => {
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -59,9 +59,9 @@ function Main(): JSX.Element {
         <img src={headshot} className="headshot-img" alt="Alec Thibodeau headshot" />
         <div className="section-title">{helpers.formatTitleCase(sections.about)}</div>
         <p>
-          Hello. I'm a software developer and a visual artist. My specialty is front-end
-          web development with a focus on the user interface (UI), the user experience (UX),
-          digital accessibility and responsive web design. I'm presently exploring ways to
+          Hello. I'm a software developer and a visual artist. My work in front-end
+          web development focuses on the user interface (UI), the user experience (UX),
+          digital accessibility and responsive web design. I enjoy looking for new ways to
           combine coding and visual expression.
         </p>
       </section>
@@ -80,10 +80,10 @@ function Main(): JSX.Element {
         <div className="section-title">{helpers.formatTitleCase(sections.projects)}</div>
         <p>
           My personal projects include four web applications built with React, TypeScript and Sass.
-          These websites contain original content and are live and interactive &mdash; and their code is
+          These are live and interactive &mdash; and their code is
           viewable at <a href={hypertextReference.gitHub} className="text-link">GitHub</a>.
-          I also handle the UI and UX design for these sites, with features including semantic HTML,
-          accessible elements, mobile-first responsiveness, custom color palettes and unique SVG images.
+          I also handle their UI and UX design: with features like semantic HTML,
+          accessible elements, mobile-first responsiveness and custom color palettes.
         </p>
         <ul>
           {projectItems.slice(0, 3).map(renderProjectDescription)}
