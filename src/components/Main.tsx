7f79888b874f22223925ab42ec76ff6d210c1b47
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-06-11 06:06:26 -0400

    feat: update projects to temporarily remove nona-ai in anticipation of addressing its breaking changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,7 +15,7 @@ function Main(props: { isBreakpointXs: boolean, viewportWidth: number }): JSX.El
   const { projectItems, sections } = constants;
   const { formatTitleCase } = helpers;
 
-  const selectProjects = projectItems.slice(0, 4);
+  const selectProjects = projectItems.slice(1, 4);
 
   /**
    * Spread attributes can be useful but they also make it easy to pass unnecessary props
