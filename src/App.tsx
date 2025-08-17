704801887d816e9d0b496df47d99393d118c1468
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-08-17 18:32:56 -0400

    feat: add boolean variable to enable redirect

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,40 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 /* Components */
 import CodePortfolio from './components/CodePortfolio';
 import NotFound from './components/NotFound';
+import Loader from './components/Loader';
 
 function App(): JSX.Element {
+  const isRedirectActive: boolean = true;
+
+  if (isRedirectActive) {
+    window.setTimeout(function() {
+      window.location.href = "http://www.alec.art";
+    }, 5000);
+  }
 
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<CodePortfolio />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      {
+        isRedirectActive ?
+        <main className="redirect-main">
+          <div className="redirect-message-wrapper">
+            <p className="redirect-message">
+              Hi. You're being redirected
+              to <a href="https://www.huetown.com" className="text-link">huetown.com</a>.
+            </p>
+            <Loader />
+          </div>
+        </main> :
+        <BrowserRouter>
+          <div className="app">
+            <Routes>
+              <Route path="/" element={<CodePortfolio />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      }
+    </>
   );
 }
 
---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+function Loader(): JSX.Element {
+  return (
+    <div className="loader-dot-container">
+      <div className="loader-dot"></div>
+    </div>
+  );
+}
+
+export default Loader;
