b608d3f4a9128599bf2b5f83b0785290f1bc415d
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2026-08-23 08:20:39 -0400

    feat: add new link to reference design content

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -57,7 +57,7 @@ function Main(props: { isBreakpointXs: boolean, viewportWidth: number }): JSX.El
             <h3 className="section-title">{formatTitleCase(sections.about)}</h3>
             <p>
               When I'm not writing software for interactive projects I'm
-              crafting designs with various techniques. My software
+              crafting <a className="text-link" href="https://alect.me/design">designs</a> with various techniques. My software
               focus is front-end web development, the user experience, user interfaces,
               digital accessibility and responsive web design.
             </p>
